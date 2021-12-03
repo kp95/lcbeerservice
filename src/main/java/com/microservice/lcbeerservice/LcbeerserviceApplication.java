a4d6d22f344de65237b81f3881ccb5f0d2f414da
kp95 <penduanttal@gmail.com>
2021-12-02 22:17:42 -0500

    Initial commit

---
+++ src/main/java/com/microservice/lcbeerservice/LcbeerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.lcbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcbeerserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcbeerserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/microservice/lcbeerservice/LcbeerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.lcbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcbeerserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
